fb941cb3bd9892c2986a7413d726db9a988037f8
Kailan Dias <kleber@example.com>
2025-07-17 17:20:47 -0300

    Initial commit

---
+++ src/main/java/Model/Usuario.java
@@ -0,0 +1,20 @@
+package Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "usuarios")
+public class Usuario {
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/githuh/kailandias/agendaviagens/AgendaViagensApplication.java
@@ -0,0 +1,13 @@
+package com.githuh.kailandias.agendaviagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaViagensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaViagensApplication.class, args);
+    }
+
+}
