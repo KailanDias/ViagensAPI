dc0332dc2e8d6a0fe03816e08359fc8a4599662b
Kailan Dias <kailanlopes1103@gmail.com>
2025-07-17 17:20:47 -0300

    Initial commit

---
+++ src/test/java/com/githuh/kailandias/agendaviagens/AgendaViagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githuh.kailandias.agendaviagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaViagensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
