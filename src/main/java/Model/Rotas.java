f2e46b0389a4ed7aa1eeea1064a74f02b57c9eb1
Kailan Dias <kleber@example.com>
2025-07-18 13:53:41 -0300

    Criaçao Entidade Rotas

---
+++ src/main/java/Model/Rotas.java
@@ -0,0 +1,23 @@
+package Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "rotas")
+public class Rotas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRota;
+
+    @Column(name = "ponto_partida", nullable = false)
+    private String pontoPartida;
+
+    @Column(name = "cidade_destino", nullable = false)
+    private String cidadeDestino;
+
+
+
+}
