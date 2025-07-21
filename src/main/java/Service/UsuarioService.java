77dc90b8ed996b4ddff0b9553b905b1772ed4fb3
Kailan Dias <kleber@example.com>
2025-07-21 11:02:05 -0300

    Criacao UsuarioController

--- src/main/java/Service/UsuarioService.java
+++ src/main/java/Service/UsuarioService.java
@@ -5,6 +5,8 @@ import Repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UsuarioService {
 
@@ -14,6 +16,15 @@ public class UsuarioService {
 
     public Usuario salvar(Usuario usuario){
         return usuarioRepository.save(usuario);
+    }
+
+    public Usuario deletar(Usuario usuario){
+        usuarioRepository.delete(usuario);
+        return usuario;
+    }
+
+    public List<Usuario> listarTodos(){
+        return usuarioRepository.findAll();
     }
 
 }
