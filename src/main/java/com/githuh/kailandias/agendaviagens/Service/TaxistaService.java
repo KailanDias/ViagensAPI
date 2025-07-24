9b94b65f53f71fdaf1e7a1d04f4b309f61f2f2c3
Kailan Dias <kailanlopes1103@gmail.com>
2025-07-24 16:44:12 -0300

    Criacao TaxistaService

---
+++ src/main/java/com/githuh/kailandias/agendaviagens/Service/TaxistaService.java
@@ -0,0 +1,29 @@
+package com.githuh.kailandias.agendaviagens.Service;
+
+import com.githuh.kailandias.agendaviagens.Model.Taxista;
+import com.githuh.kailandias.agendaviagens.Model.Usuario;
+import com.githuh.kailandias.agendaviagens.Repository.TaxistasRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaxistaService {
+
+    public TaxistasRepository taxistasRepository;
+
+    public Taxista salvar(Taxista taxista){
+        return taxistasRepository.save(taxista);
+    }
+
+    public Taxista deletar(Taxista taxista){
+        taxistasRepository.delete(taxista);
+        return taxista;
+    }
+
+    public List<Taxista> listarTodos(){
+        return taxistasRepository.findAll();
+    }
+
+
+}
