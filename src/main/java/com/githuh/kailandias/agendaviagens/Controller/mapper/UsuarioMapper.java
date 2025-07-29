3b593b6dcd1788ce32b62ffd1d35b9066c4038ac
Kailan Dias <kleber@example.com>
2025-07-29 16:39:56 -0300

    Correcao UsuarioMapper -  2

--- src/main/java/com/githuh/kailandias/agendaviagens/Controller/mapper/UsuarioMapper.java
+++ src/main/java/com/githuh/kailandias/agendaviagens/Controller/mapper/UsuarioMapper.java
@@ -9,17 +9,9 @@ import org.mapstruct.MappingTarget;
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
-    // Adicione esta anotação para corrigir o erro
-    @Mapping(source = "idUsuario", target = "id")
     UsuarioDTO toDto(Usuario usuario);
 
-    @Mapping(target = "idUsuario", ignore = true)
     Usuario toEntity(UsuarioDTO dto);
 
-//    @Mapping(target = "id", ignore = true)
-//    @Mapping(target = "email", ignore = true) // Ignora campos que não devem ser atualizados a partir do DTO
-//    @Mapping(target = "senha", ignore = true)
-//    @Mapping(target = "dataCadastro", ignore = true)
-//    void updateEntityFromDto(UsuarioDTO dto, @MappingTarget Usuario usuario);
 
 }
