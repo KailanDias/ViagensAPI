8a46159c5e8f65b42ef5b63855c523789dda9be3
Kailan Dias <kailanlopes1103@gmail.com>
2025-07-29 16:02:48 -0300

    Correçao mapper

--- src/main/java/com/githuh/kailandias/agendaviagens/Controller/mapper/UsuarioMapper.java
+++ src/main/java/com/githuh/kailandias/agendaviagens/Controller/mapper/UsuarioMapper.java
@@ -4,13 +4,22 @@ import com.githuh.kailandias.agendaviagens.Controller.dto.UsuarioDTO;
 import com.githuh.kailandias.agendaviagens.Model.Usuario;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
+    // Adicione esta anotação para corrigir o erro
+    @Mapping(source = "idUsuario", target = "id")
     UsuarioDTO toDto(Usuario usuario);
 
-    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "idUsuario", ignore = true)
     Usuario toEntity(UsuarioDTO dto);
+
+//    @Mapping(target = "id", ignore = true)
+//    @Mapping(target = "email", ignore = true) // Ignora campos que não devem ser atualizados a partir do DTO
+//    @Mapping(target = "senha", ignore = true)
+//    @Mapping(target = "dataCadastro", ignore = true)
+//    void updateEntityFromDto(UsuarioDTO dto, @MappingTarget Usuario usuario);
 
 }
--- src/main/java/com/githuh/kailandias/agendaviagens/Service/TaxistaService.java
+++ src/main/java/com/githuh/kailandias/agendaviagens/Service/TaxistaService.java
@@ -3,6 +3,7 @@ package com.githuh.kailandias.agendaviagens.Service;
 import com.githuh.kailandias.agendaviagens.Model.Taxista;
 import com.githuh.kailandias.agendaviagens.Model.Usuario;
 import com.githuh.kailandias.agendaviagens.Repository.TaxistasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @Service
 public class TaxistaService {
 
+    @Autowired
     public TaxistasRepository taxistasRepository;
 
     public Taxista salvar(Taxista taxista){
